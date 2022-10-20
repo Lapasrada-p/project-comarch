0128d7fbb429a73f5cbad8a28d8d6dfcdcb950b4
minmeeen <sirapat.mp@gmail.com>
2022-10-20 21:08:39 +0700

    update

--- assembler.py
+++ assembler.py
@@ -1,8 +1,7 @@
 import sys
-#### Still don't have checking reg after instruct ####
 
-fname = "tester.txt"
-assem = []
+fname = "multiply.txt"
+assem = []      #collect assambly
 
 try:
     f = open(fname, 'r')
@@ -17,46 +16,33 @@ with f:
     f.close()
 
 machine_c = []
-dec = []
+dec = []        
 label = []
-opcode = []
+opcode = []     #collect opcode
 ins = ['add', 'nand', 'lw', 'sw', 'beq', 'jalr', 'halt', 'noop']
-rs = []
-rt = []
-rd = []
-f = []
+rs = []     #collect rs
+rt = []     #collect rt
+rd = []     #collect rd or offset
+f = []      #collect value in .fill
 
-for i in range(len(assem)): #loop assemble code
+for i in range(len(assem)): #loop assembly code
     x = assem[i].split() 
-    # print(x)
     
-    if x[0] in ins: #In case code doesn't have label
-        if x[0] =='add' or x[0] =='nand' :
+    if x[0] in ins:     #In case code doesn't have label
+        #checking R-type
+        if x[0] =='add' or x[0] =='nand' :      
             if x[0] =='add':
                 opcode.append('000')
             elif x[0] == 'nand':
                 opcode.append('001')
             
-            
-            #why use this got invalid syntax ;-;
-            # match x[0]:
-            #     case 'add':
-            #         opcode.append('000')
-            #     case 'nand':
-            #         opcode.append('001')
-            #     case 'lw':
-            #         opcode.append('010')
-            #     case 'sw':
-            #         opcode.append('011')
-            #     case 'beq':
-            #         opcode.append('100')
             rs.append(x[1])
             rt.append(x[2])
             rd.append(x[3])
             f.append(None)
 
-            # print(rd[i])
-        elif x[0] =='lw' or x[0] =='sw' or x[0] =='beq':
+        #checking I-type
+        elif x[0] =='lw' or x[0] =='sw' or x[0] =='beq':        
             if x[0] == 'lw':
                 opcode.append('010')
             elif x[0] == 'sw':
@@ -64,30 +50,25 @@ for i in range(len(assem)): #loop assemble code
             elif x[0] == 'beq':
                 opcode.append('100')
 
-            # print('x3', x[3])
-
             rs.append(x[1])
             rt.append(x[2])
-            rd.append(x[3])
+            rd.append(x[3])     #append offfset
             f.append(None)
 
-        elif x[0] == 'jalr':
+        #checking J-type
+        elif x[0] == 'jalr':        
             opcode.append('101')
             rs.append(x[1])
-            rd.append(None)
+            rd.append(None)         #null bcuz j-type doesn't have rd or offset
             rt.append(x[2])
             f.append(None)
-        elif x[0] == 'halt' or x[0]=='noop':
+        
+        #checking O-type
+        elif x[0] == 'halt' or x[0]=='noop':        
             if x[0] =='halt':
                 opcode.append('110')
             elif x[0] == 'noop':
                 opcode.append('111')
-
-            # match x[0]:
-            #     case 'halt':
-            #         opcode.append('110')
-            #     case 'noop':
-            #         opcode.append('111')
 
             rs.append(None)
             rt.append(None)
@@ -96,36 +77,27 @@ for i in range(len(assem)): #loop assemble code
         
         label.append(None) #Set label[i] is null
 
-    elif len(x[0]) >6 or x[0][0].isdigit() or x[0] in label: #Check label
-        print("Error: Label error")
+    elif len(x[0]) >6 or x[0][0].isdigit() or x[0] in label: #Check label more than 6 charactor, or starting char is number
+        # print("Error: Label error")
         sys.exit(1)
-    elif(len(x)) == 1 :
+    elif(len(x)) == 1 :     #checking pc has only label
         print("Error: Label undefine")
         sys.exit(1)
-    else:
+    else:       #In case present pc have label
         label.append(x[0])
+        #R-type
         if x[1] =='add' or x[1] =='nand' :
             if x[1] =='add':
                 opcode.append('000')
             elif x[1] == 'nand':
                 opcode.append('001')
             
-            
-            # match x[1]:
-            #     case 'add':
-            #         opcode.append('000')
-            #     case 'nand':
-            #         opcode.append('001')
-            #     case 'lw':
-            #         opcode.append('010')
-            #     case 'sw':
-            #         opcode.append('011')
-            #     case 'beq':
-            #         opcode.append('100')
             rs.append(x[2])
             rt.append(x[3])
             rd.append(x[4])
             f.append(None)
+
+        #I-type
         elif x[1] =='lw' or x[1] =='sw' or x[1] =='beq':
             if x[1] == 'lw':
                 opcode.append('010')
@@ -136,7 +108,7 @@ for i in range(len(assem)): #loop assemble code
 
             rs.append(x[2])
             rt.append(x[3])
-            rd.append(x[4])
+            rd.append(x[4])     #append offset
             f.append(None)
         
         #JALR
@@ -147,17 +119,13 @@ for i in range(len(assem)): #loop assemble code
             rt.append(x[3])
             f.append(None)
 
-        #halt or noop
+        #halt or noop O-type
         elif x[1] == 'halt' or x[1]=='noop':
             if x[1] =='halt':
                 opcode.append('110')
             elif x[1] == 'noop':
                 opcode.append('111')
-            # match x[1]:
-            #     case 'halt':
-            #         opcode.append('110')
-            #     case 'noop':
-            #         opcode.append('111')
+
             rs.append(None)
             rt.append(None)
             rd.append(None)
@@ -165,9 +133,6 @@ for i in range(len(assem)): #loop assemble code
 
         #.fill
         elif x[1] == '.fill':
-            # print(rd)
-            # print(label)
-            # print(len(x))
             if(len(x) < 3):
                 print("Error")
                 sys.exit(1)
@@ -176,142 +141,118 @@ for i in range(len(assem)): #loop assemble code
             rs.append(None)
             rt.append(None)
             rd.append(None)
-            # print('x[2]',x[2])
-            # print(x[2] in label)
-
              
-            if x[2] in label and not(x[0] in rd):     #if  after .fill is in label
-                index = label.index(x[2])       #finding index of x[2] in label list
-                f.append(index)
-                for j in range(len(rd)):
-                    if rd[j] == x[0]:
-                        rd[j] = i
-            elif x[0] in rd and not(x[2] in label):      #if label have the same name in rd
-                for j in range(len(rd)):
-                    if rd[j] == x[0]:
-                        rd[j] = i
+            if x[2] in label and not(x[0] in rd):     #if after .fill is in label and doesn't have in rd[]
+                index = label.index(x[2])       #finding index of .fill's value in label list
+                f.append(index)                 #add index of .fill's value in label to .fill list
+        
+            elif x[0] in rd and not(x[2] in label):      #if label have the same name in rd[] and .fill's value isn't in label
+                for j in range(len(rd)):        #loop in rd list 
+                    if rd[j] == x[0]:           #if offset in rd is same as label
+                        rd[j] = i               #change offset from symbolic to present address
                 f.append(x[2])
-                # print(2)
-            elif x[0] in rd and x[2] in label:      
-                l_index = label.index(x[2])         
-                # print("index", l_index)
-                # r_index = rd.index(x[0])
+                
+            elif x[0] in rd and x[2] in label:      #if rd/offset have value as same as label and .fill's value is in label
+                l_index = label.index(x[2])         #find index of .fill's value that's in label list 
 
                 f.append(l_index)
                 for j in range(len(rd)):
                     if rd[j] == x[0]:
                         rd[j] = i
-                        # rd[j] = l_index
-                # print(3)
+                        
             else:
                 f.append(x[2])
-                # print(4)
-        else:
+
+        else:       #In case pc have other instruction
             print("Opcode Error: Do not have this instruction")
             sys.exit(1)
 
 
-
-# print("rd",rd)
-# print("f",f)
-
 #checking beq if rd is symbolic
-for i in range(len(rd)):
-    print(f)
+for i in range(len(rd)):        
     s = str(rd[i])
     if(rd[i] != None):
-        if s[0].isalpha():
-                    if rd[i] in label:
-                        ind = label.index(rd[i])
-                        # print("ind",ind)
-                        # print("i",i)
-                        rd[i] = ind - i -1 
+        if s[0].isalpha():      #if offsetField is symbolic
+                    if rd[i] in label:      #if offset is in label
+                        ind = label.index(rd[i])        #find index that offset has the same name as label[]
+                        rd[i] = ind - i -1      #edit offset
                        
-                        # print("rd",rd[i])
                     
-
-
-w = open("input_simulator.txt","w")     #open filewriting for simulator
+w = open("input_simulator.txt","w")     #open file writing for simulator
 
 
 for i in range(len(assem)): #loop for generate bin and dec
     
-    # print(i)
     isFill = False
     
     # R-type
     if(opcode[i] == '000' or opcode[i] == '001'):
-        # print(rd[i][0])
+
         s = str(rd[i])
-        if s[0].isalpha():
-            print("Cannot use symbolic in rd")
+        if s[0].isalpha():      #checking rd is symbolic
+            print("Cannot use symbolic in rd of R-type")
             sys.exit(1)
 
-        rs[i] =  bin(int(rs[i]))[2:].zfill(3)
+        #Changing decimal to 3 bit binary 
+        rs[i] =  bin(int(rs[i]))[2:].zfill(3)   
         rt[i] =  bin(int(rt[i]))[2:].zfill(3)
         rd[i] =  bin(int(rd[i]))[2:].zfill(3)
 
-        # print(rs[i],rt[i],rd[i])
         m = opcode[i]+rs[i]+rt[i]+'0000000000000'+rd[i]
-        # print(m)
         
     # I-Type
     elif (opcode[i] == '010' or opcode[i] == '011' or opcode[i] == '100') :
+        #Changing decimal to 3 bit binary
         rs[i] =  bin(int(rs[i]))[2:].zfill(3)
         rt[i] =  bin(int(rt[i]))[2:].zfill(3)
-        # print('i',i ,'rd',rd[i])
+
         if int(rd[i])>32767 or int(rd[i])<-32768:
             print(f"Error: In address {i}, offsetField is more than 16 bit")
             sys.exit(1)
-        elif(int(rd[i])<0):
+        elif(int(rd[i])<0):     #If offset is negative number
+            #Changing decimal to 16 bit binary
             rd[i] =  bin(int(rd[i])&0b1111111111111111)[2:].zfill(16)
-        else:     
+        else:     #If offset is pos
             rd[i] =  bin(int(rd[i]))[2:].zfill(16)
 
         m = opcode[i]+rs[i]+rt[i]+rd[i]
-        # print(m)
+
         
     # J-Type
     elif (opcode[i] == '101') :
+        #Changing decimal to 3 bit binary
         rs[i] =  bin(int(rs[i]))[2:].zfill(3)
         rt[i] =  bin(int(rt[i]))[2:].zfill(3)
 
-        # print(rs[i],rt[i],rd[i])
         m = opcode[i]+rs[i]+rt[i]+'0000000000000000'
-        # print(m)
     
     # O-Type
     elif (opcode[i] == '110' or opcode[i] == '111'):
         m= opcode[i] + '0000000000000000000000'
 
+    # .fill
     else:
-        # print(f)
         isFill = True
         m = f[i]
 
 
     machine_c.append((m)) 
-    if(isFill == False):
-        decimal = int(machine_c[i], 2)
+    if(isFill == False):        #instruction is not .fill
+        decimal = int(machine_c[i], 2)      #Convert bin to dec
     else:
         decimal = machine_c[i]
     dec.append(decimal)
     
-    
-    
-# print(label)
-
 
 #Printing
 for i in range(len(assem)):
-    print(rs[i],rt[i],rd[i])   #easy for checking
+    # print(rs[i],rt[i],rd[i])   #easy for checking
     print(f"(address {i}): {dec[i]}" ) #print decimal of code line by line
     
-
     #write decimal in file for simulator
     w.write(str(dec[i]))
     if i != len(assem)-1:
         w.write('\n')
 
-w.close()
+w.close()       #close writer
 sys.exit(0) #end program
--- simulator.py
+++ simulator.py
@@ -11,147 +11,109 @@ except OSError:
 
 with f:
     for x in f:
-        fromAssem.append(x)
+        y = x.rstrip()      #For deleting /n in line
+        fromAssem.append(int(y))
     f.close()
 
-dec = []
-# #convert dec to bin
-for i in range(len(fromAssem)):
-    x = fromAssem[i].split()
-    print(x)
-    dec.append(int(x[0]))
-
-# print(dec)
-# print(type(dec[0]))
-# print(bin(dec[0]).replace("0b", ""))
 
 machine_c = []
-for i in range(len(dec)):
-    b = bin(dec[i]).replace("0b", "").zfill(25) #แก้ให้เป็น 25 bit
+for i in range(len(fromAssem)):
+    b = bin(fromAssem[i]).replace("0b", "").zfill(25) #Convert decimal to binary
     machine_c.append(b)
    
-# print(dec) 
-# print(machine_c)
-
 
 mem = []
 reg = [0,0,0,0,0,0,0,0] #set all reg to 0 in first
 
 for i in range(len(fromAssem)): #loop showing what's inside mem
-    mem.append(fromAssem[i])
+    mem.append(fromAssem[i])        #memory = address
     
+w = open("output_simulator.txt","w")
 
-#printing
-#output
-for i in range(len(mem)): #loop for showing what's inside mem
-    print(f"memory[{i}]={mem[i]}")
-
+#stateStruct
 pc = 0
 numMemory = len(mem)
 count = 0
 
 
-w = open("output_simulator.txt","w")
+#printing
+#output
+for i in range(len(mem)): #loop for writing what's inside mem
+    w.write(f"memory[{i}]={mem[i]}\n")
+w.write("\n")
 
 
 while( pc < numMemory):
-    # print()
-    # print("@@@")
-    # print("state:")
-    # print(f"\tpc {pc}")
-    # print("\tmemory: ")
-    # for j in range(numMemory):
-    #     print(f"\t\tmem[ {j} ] {mem[j]}")
-    # print("\tregisters:")
-    # for k in range(len(reg)):
-    #     print(f"\t\treg[ {k} ] {reg[k]}")
-    # print("end state")
-    # print()
-
-    # print('reg1',reg[1])
-
+   
     #For writing output
+    #PrintState
     w.write('\n')
     w.write("@@@\n")
     w.write("state:\n")
     w.write(f"\tpc {pc}\n")
     w.write("\tmemory: \n")
     for j in range(numMemory):
-        w.write(f"\t\tmem[ {j} ] {mem[j]}\n")
-    w.write("\tregisters:\n")
+        w.write(f"\t\t\tmem[ {j} ] {mem[j]}\n")
+    w.write("\n\tregisters:\n")
     for k in range(len(reg)):
-        w.write(f"\t\treg[ {k} ] {reg[k]}\n")
-    w.write("end state\n")
+        w.write(f"\t\t\treg[ {k} ] {reg[k]}\n")
+    w.write("end state\n")    
 
-    w.write('\n')
-    w.write("@@@\n")
-    w.write("state:\n")
-    w.write(f"\tpc {pc}\n")
-    w.write("\tmemory: \n")
-    for j in range(numMemory):
-        w.write(f"\t\tmem[ {j} ] {mem[j]}\n")
-    w.write("\tregisters:\n")
-    for k in range(len(reg)):
-        w.write(f"\t\treg[ {k} ] {reg[k]}\n")
-    w.write("end state\n")
 
-    
-
-    opcode = machine_c[pc][0:3] 
+    opcode = machine_c[pc][0:3]         #Opcode = bits 22-24
 
 
     #### Add ####
     if opcode == '000' :
-        # print("add")
-        rs = int(machine_c[pc][3:6],2)
-        rt = int(machine_c[pc][6:9],2)
-        rd = int(machine_c[pc][22:25],2)
-        reg[rd] = reg[rs] + reg[rt]
-        pc+=1
+        #Convert binary to decimal
+        rs = int(machine_c[pc][3:6],2)      #bits 21-19
+        rt = int(machine_c[pc][6:9],2)      #bits 18-16
+        rd = int(machine_c[pc][22:25],2)    #bits 2-0
+        reg[rd] = reg[rs] + reg[rt]         #value in rd = value in rs+rd
+        pc+=1   #Next pc
 
     #### Nand ####
     elif opcode == '001' :
-        print("nand")
-        s = ''
-        rs = int(machine_c[pc][3:6],2)
-        rt = int(machine_c[pc][6:9],2)
-        rd = int(machine_c[pc][22:25],2)
+        # print("nand")
+        s = ''      #String for answer of nand
+        #Convert binary to decimal
+        rs = int(machine_c[pc][3:6],2)      #bits 21-19
+        rt = int(machine_c[pc][6:9],2)      #bits 18-16
+        rd = int(machine_c[pc][22:25],2)    #bits 2-0
         
-        n1 = bin(reg[rs])[2:].zfill(16)
-        # print(bin(reg[rt]))
+        #convert value in rs to 16 bits binary
+        if(int(reg[rs])<0):     #If value in rs is negative number
+            #Changing decimal to 16 bit binary
+            n1 =  bin((reg[rs])&0b1111111111111111)[2:].zfill(16)
+        else:     #positive
+            n1 = bin(reg[rs])[2:].zfill(16)
+
+        if(reg[rt]<0):     #If value in rt is negative number
+            #Changing decimal to 16 bit binary
+            n2 =  bin((reg[rt])&0b1111111111111111)[2:].zfill(16)
+        else:     #positive
+            n2 = bin(reg[rt])[2:].zfill(16)
    
-        n2 = bin(reg[rt])[2:].zfill(16)
-        # print(n1,n2)
-
-
         s = ''
-        for i in range(len(n1)):        #loop each character, nand each other
-            if n1[i] == '1' and n2[i] == '1':
-                # nand.append('0')
+        for i in range(len(n1)):        #loop each character and nand each other
+            if n1[i] == '1' and n2[i] == '1':       #If rs = 1 and rt = 1
                 s = s+'0'
             else:
-            # nand.append('1')
                 s = s+'1'
 
-        reg[rd]= int(s,2)       #bin to dec then save to reg[rd]
-        # print('n1',n1)
-        # print('n2',n2)
-        # print('s ',s)
-
+        reg[rd] = int(s,2)       #Convert binary to decimal then save to reg[rd]
 
         pc+=1
 
     #### Lw ####
     elif opcode == '010':
-        # print("lw")
-        rs = int(machine_c[pc][3:6],2)
-        rt = int(machine_c[pc][6:9],2)
-        offset = int(machine_c[pc][9:25],2)
+        # Convert binary to decimal
+        rs = int(machine_c[pc][3:6],2)      #bits 21-19
+        rt = int(machine_c[pc][6:9],2)      #bits 18-16
+        offset = int(machine_c[pc][9:25],2) #bits 15-0
         
-
         addr = int(offset) + reg[rs]        #Finding address
-        # print(addr)
-        # print('offset',offset)
+
         reg[rt] = int(mem[addr])    #store reg of rt to pc that give from value in [offset+rs]
 
         pc+=1
@@ -159,14 +121,12 @@ while( pc < numMemory):
 
     #### Sw ####
     elif opcode == '011':
-        print("sw")
-        rs = int(machine_c[pc][3:6],2)
-        rt = int(machine_c[pc][6:9],2)
-        offset = int(machine_c[pc][9:25],2)
+        #Convert binary to decimal
+        rs = int(machine_c[pc][3:6],2)      #bits 21-19
+        rt = int(machine_c[pc][6:9],2)      #bits 18-16
+        offset = int(machine_c[pc][9:25],2) #bits 15-0
         
         addr = int(offset) + reg[rs]
-        # print('offset',offset)
-        # w.write(str(offset))
         
         if(addr < numMemory):
             mem[addr] = reg[rt] #store reg of rt to pc that give from value in [offset+rs]
@@ -182,67 +142,46 @@ while( pc < numMemory):
 
     #### Beq ####    
     elif opcode == '100':
-        # print("beq")
-        rs = int(machine_c[pc][3:6],2)
-        rt = int(machine_c[pc][6:9],2)
-        #To convert 2s component
-        if(machine_c[pc][9] == '1'):    #negative
-            m = machine_c[pc][9:25] 
+        # Convert decimal to binary
+        rs = int(machine_c[pc][3:6],2)      #bits 21-19
+        rt = int(machine_c[pc][6:9],2)      #bits 18-16
+
+        # Convert 2's complement
+        if(machine_c[pc][9] == '1'):        #negative
+            m = machine_c[pc][9:25]         #bits 15-0
             o = (int(m,2)^0b1111111111111111) +1
             m = o*(-1)
             
             offset = m
         else:                           #positive
-            offset = int(machine_c[pc][9:25],2)
+            offset = int(machine_c[pc][9:25],2) #bits 15-0
 
-        # print('offset',offset)
-        if reg[rs] == reg[rt]:
-            # print(i)
+        if reg[rs] == reg[rt]:      #if value in rs and rt is the same
             pc = pc + 1 + offset    #Set new pc to jump
-            # print(i)
         else:
             pc+=1
 
-        # print('pc',pc) 
-
     #### Jalr ####
     elif opcode == '101':
-        print("jalr")
-        rs = int(machine_c[pc][3:6],2)
-        rt = int(machine_c[pc][6:9],2)
+        #Convert binary to decimal
+        rs = int(machine_c[pc][3:6],2)      #bits 21-19
+        rt = int(machine_c[pc][6:9],2)      #bits 18-16
         reg[rt] = pc+1
         if(rs != rt):
             pc = reg[rs]
             
     #### Halt ####
     elif opcode == '110':
-        print("halt")
+        # print("halt")
         break
 
     #### Noop ####
     elif opcode == '111':
         #Do nothing
-        print("noop")
+        # print("noop")
         pc+=1
 
-    
-    count += 1  #counting instructions executed
-
-print('machine halted')
-print(f'total of {count+1} instructions executed')
-print('final state of machine:')
-print()
-print("@@@")
-print("state:")
-print(f"\tpc {pc+1}")
-print("\tmemory: ")
-for j in range(numMemory):
-    print(f"\t\tmem[ {j} ] {mem[j]}")
-print("\tregisters:")
-for k in range(len(reg)):
-    print(f"\t\treg[ {k} ] {reg[k]}")
-print("end state")
-
+    count += 1  #counting instructions that was executed
 
 
 w.write('machine halted\n')
@@ -251,13 +190,13 @@ w.write('final state of machine:\n')
 w.write('\n')
 w.write("@@@\n")
 w.write("state:\n")
-w.write(f"\tpc {pc+1}\n")
+w.write(f"\tpc {pc}\n")
 w.write("\tmemory: \n")
 for j in range(numMemory):
-    w.write(f"\t\tmem[ {j} ] {mem[j]}\n")
-w.write("\tregisters:\n")
+    w.write(f"\t\t\tmem[ {j} ] {mem[j]}\n")
+w.write("\n\tregisters:\n")
 for k in range(len(reg)):
-    w.write(f"\t\treg[ {k} ] {reg[k]}\n")
+    w.write(f"\t\t\treg[ {k} ] {reg[k]}\n")
 w.write("end state\n")
 
 sys.exit(0) #end program
